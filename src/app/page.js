59a97ed07b54a5e16ec3535a40bf6e375116c750
ogtitle724 <wje0724@gmail.com>
2024-12-09 17:29:03 +0900

    TEST PR

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>testt</div>;
+  return <div className={styles.page}>또바꿈꿈</div>;
 }
